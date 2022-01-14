49459756b9f178962f61cf7253ad85e0b3c21ce0
ShaliniBarmanRoy <shalini.barmanroy@gmail.com>
2022-01-14 16:48:58 +0530

    add new class

---
+++ src/SampleProject/Gitcode.java
@@ -0,0 +1,12 @@
+package SampleProject;
+
+public class Gitcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum =98;
+		System.out.println(sum);
+	}
+
+}
