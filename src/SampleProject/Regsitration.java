6613a8aee66589530bbf785d2f4ff224d2ad9556
ShaliniBarmanRoy <shalini.barmanroy@gmail.com>
2022-01-14 12:31:25 +0530

    added new files

---
+++ src/SampleProject/Regsitration.java
@@ -0,0 +1,12 @@
+package SampleProject;
+
+public class Regsitration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("For New Commit");
+
+	}
+
+}
